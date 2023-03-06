0304494ff8b6a5720181b6ea824ec6a8efc9d06b
naoto575 <snao7010@icloud.com>
2023-03-06 23:39:38 +0900

    second commit

---
+++ user_config_template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# The base directory for model output.
+export JIANT_PROJECT_PREFIX="output/directory"
+
+# Base directory in which to look for raw data subdirectories. This
+# could be the glue_data directory created by download_glue_data.py.
+export JIANT_DATA_DIR="your/dataset/directory"
